7e6452b442a9bca8d3cd256f754da4e72201e7bd
Oiching-SuiLin <13425535649@163.com>
2018-07-30 00:35:34 +0800

    add src

---
+++ src/bin/js/LayaUISample.js
@@ -0,0 +1,406 @@
+﻿var Loader = laya.net.Loader;
+var Handler = laya.utils.Handler;
+var WebGL = laya.webgl.WebGL;
+
+var x = 0;
+var y = 0;
+var RN = 4;
+var CN = 4;
+var score = 0;
+var state = 1;
+var movex = 0;
+var movey = 0;
+var RUNNING = 1;
+var data = null;
+var GAMEOVER = 0;
+var gamebox = null;
+
+function changeboxcolor ( i, j ){
+	if ( data[i][j] === 2 ){
+		gamebox[i][j].color = "#776e65";
+		gamebox[i][j].bgColor = "#eee3da";
+	}
+	else if ( data[i][j] === 4 ){
+		gamebox[i][j].color = "#776e65";
+		gamebox[i][j].bgColor = "#ede0c8";
+	}
+	else if ( data[i][j] === 8 ){
+		gamebox[i][j].bgColor = "#f2b179";
+	}
+	else if ( data[i][j] === 16 ){
+		gamebox[i][j].bgColor = "#f59563";
+	}
+	else if ( data[i][j] === 32 ){
+		gamebox[i][j].bgColor = "#f67c5f";
+	}
+	else if ( data[i][j] === 64 ){
+		gamebox[i][j].bgColor = "#f65e3b";
+	}
+	else if ( data[i][j] === 128 ){
+		gamebox[i][j].bgColor = "#edcf72";
+	}
+	else if ( data[i][j] === 256 ){
+		gamebox[i][j].bgColor = "#edcc61";
+	}
+	else if ( data[i][j] === 512 ){
+		gamebox[i][j].bgColor = "#9c0";
+	}
+	else if ( data[i][j] === 1024 ){
+		gamebox[i][j].bgColor = "#33b5e5";
+	}
+	else if ( data[i][j] === 2048 ){
+		gamebox[i][j].bgColor = "#09c";
+	}
+	else if ( data[i][j] === 4096 ){
+		gamebox[i][j].bgColor = "#a6c";
+	}
+} 
+
+function randomNum(){
+	while ( 1 )
+	{
+		var i = Math.floor(Math.random() * RN);
+		var j = Math.floor(Math.random() * CN);
+		if ( data[i][j] === 0 ){
+			data[i][j] = Math.random() < 0.6 ? 2 : 4;
+			break;			
+		}
+	}
+}
+
+function updataView(){
+	for ( var i = 0; i < RN; i++ ){
+		for ( var j = 0; j < CN; j++ ){
+			if ( data[i][j] !== 0 )
+			{
+				gamebox[i][j].text = data[i][j];
+				changeboxcolor(i, j);
+			}
+			else
+			{
+				gamebox[i][j].text = "";
+				gamebox[i][j].bgColor = "#ccc0b3";
+			}
+		}
+	}
+	Testui.score.text = "分数：" + score;
+	if ( state === GAMEOVER ){
+		Testui.stop.visible = false;
+		Testui.score.visible = false;
+		Testui.box2.visible = true;
+		Testui.gamescore.visible = true;
+		Testui.gamescore.text = "你最后的分数为：" + score;
+		Testui.gamescore.fontSize = 30;
+	}
+}
+
+function isGameOver(){
+	for ( var i = 0; i < RN; i++ ){
+		for ( var j = 0; j < CN; j++ ){
+			if ( data[i][j] === 0 ||
+			 j < CN-1 
+			 && data[i][j] === data[i][j+1] ||
+			 i < RN-1
+			 && data[i][j] === data[i+1][j] ){
+				 return false;
+			 }
+		}
+	}
+	return true;
+}
+
+function moveUp(){
+	var before = String(data);
+	for ( var i = 0; i < RN; i++ ){
+		moveUpInRow(i);
+	}
+	var after = String(data);
+	if ( after !== before ){
+		randomNum();
+		isGameOver() && ( state = GAMEOVER );
+		updataView();
+	}
+}
+
+function getNextInRow(n1, n2){
+	for ( var i = n2 + 1; i < CN; i++ ){
+		if ( data[n1][i] !== 0 ){
+			return i;
+		}
+	}
+	return -1;
+}
+
+function moveUpInRow(num){
+	for ( var i = 0; i < CN - 1; i++ ){
+		var nexti = getNextInRow(num, i);
+		if ( nexti === -1 ){
+			break;
+		}
+		else{
+			if ( data[num][i] === 0 ){
+				data[num][i] = data[num][nexti];
+				data[num][nexti] = 0;
+				i--;
+				updataView();
+			}
+			else if ( data[num][i] === data[num][nexti] ){
+				data[num][i] *= 2;
+				score += data[num][i];
+				data[num][nexti] = 0;
+			}
+		}
+	}
+}
+
+function change1(){
+	for ( var i = 0; i < 4; i++ ){
+		for ( var j = 0; j < 2; j++ ){
+			var temp = data[i][j];
+			data[i][j] = data[i][3-j];
+			data[i][3-j] = temp;
+		}
+	}
+}
+
+function change2(){
+	for ( var i = 0; i < 4; i++ ){
+		for ( var j = i + 1; j < 4; j++ ){
+			var temp = data[i][j];
+			data[i][j] = data[j][i];
+			data[j][i] = temp;
+		}
+	}
+}
+
+function moveDown(){
+	change1();
+	moveUp();
+	change1();
+	updataView();
+}
+
+function moveLeft(){
+	change2();
+	moveUp();
+	change2();
+	updataView();
+}
+
+function moveRight(){
+	change2();
+	moveDown();
+	change2();
+	updataView();
+}
+
+function onMouseDown(){
+	movex = Laya.stage.mouseX;
+	movey = Laya.stage.mouseY;
+}
+
+function onMouseUp(){
+	x = Laya.stage.mouseX;
+	y = Laya.stage.mouseY;
+	var x1 = x - movex;
+	var y1 = y - movey;
+	x = Math.abs ( x - movex );
+	y = Math.abs ( y - movey );
+	if ( x > y && y < 100 && state === RUNNING ){
+		if ( x1 < -30){
+			moveLeft();
+		}
+		else if ( x1 > 30 ){
+			moveRight();
+		}
+	}
+	else if ( y > x && x < 100 && state === RUNNING ){
+		if ( y1 < -30 ){
+			moveUp();
+		}
+		else if ( y1 > 30 ){
+			moveDown();
+		}
+	}
+	updataView();
+}
+
+function TestUI()
+{
+	var Event = laya.events.Event;
+	TestUI.super(this);
+	this.box.on(Event.MOUSE_UP, this, onMouseUp);
+	this.stop.on(Event.CLICK, this, onstopClick);
+	this.again.on(Event.CLICK, this, onagainClick);
+	this.turn1.on(Event.CLICK, this, onturn1Click);
+	this.turn2.on(Event.CLICK, this, onturn2Click);
+	this.box.on(Event.MOUSE_DOWN, this, onMouseDown);
+	this.gamestart.on(Event.CLICK, this, ongamestartClick);
+	this.shezhi.on(Event.CLICK, this, onshezhiClick);
+	this.jia.on(Event.CLICK, this, onjiaClick);
+	this.jian.on(Event.CLICK, this, onjianClick);
+	this.turn3.on(Event.CLICK, this, onturn3Click);
+
+	function ongamestartClick()
+	{
+		this.titile1.visible = false;
+		this.titile2.visible = false;
+		this.titile3.visible = false;
+		this.titile4.visible = false;
+		this.gamestart.visible = false;
+		this.shezhi.visible = false;
+		this.box.visible = true;
+		this.stop.visible = true;
+		this.score.visible = true;
+		score = 0;
+		data = [];
+		gamebox = [];
+		state = RUNNING;
+		for ( var i = 0; i < RN; i++ )
+		{
+			data[i] = new Array( CN );
+			for ( var j = 0; j < CN; j++ )
+			{
+				data[i][j] = 0;
+			}
+		}
+		for ( var i = 0; i < RN; i++ )
+		{
+			gamebox[i] = new Array( CN );
+			for ( var j = 0; j < CN; j++ )
+			{
+				gamebox[i][j] = new Laya.Label;
+				gamebox[i][j].text = 0;
+				gamebox[i][j].x = i * 100;
+				gamebox[i][j].y = j * 100;
+				gamebox[i][j].width = 100;
+				gamebox[i][j].height = 100;
+				gamebox[i][j].fontSize = 60;
+				gamebox[i][j].color = "#fff";
+				gamebox[i][j].align = "center";
+				gamebox[i][j].bgColor = "#ccc0b3";
+				this.box.addChild(gamebox[i][j]);
+			}
+		}
+		randomNum();
+		randomNum();
+		updataView();
+	}
+
+	function onagainClick(){
+		this.box2.visible = false;
+		this.turn2.visible = false;
+		this.gamescore.visible = false;
+		this.box.visible = true;
+		this.stop.visible = true;
+		this.score.visible = true;
+		score = 0;
+		state = RUNNING;
+		for ( var i = 0; i < RN; i++ )
+		{
+			for ( var j = 0; j < CN; j++ )
+			{
+				data[i][j] = 0;
+			}
+		}
+		randomNum();
+		randomNum();
+		updataView();
+	}
+
+	function onstopClick(){
+		this.box.visible = false;
+		this.stop.visible = false;
+		this.score.visible = false;
+		this.box2.visible = true;
+		this.turn2.visible = true;
+	}
+
+	function onturn1Click(){
+		this.box.visible = false;
+		this.box2.visible = false;
+		this.turn2.visible = false;
+		this.gamescore.visible = false;
+		this.titile1.visible = true;
+		this.titile2.visible = true;
+		this.titile3.visible = true;
+		this.titile4.visible = true;
+		this.gamestart.visible = true;
+		this.shezhi.visible = true;
+		for ( var i = 0; i < RN; i++ )
+		{
+			for ( var j = 0; j < CN; j++ )
+			{
+				data[i][j] = 0;
+			}
+		}
+	}
+
+	function onturn2Click(){
+		this.box2.visible = false;
+		this.turn2.visible = false;
+		this.box.visible = true;
+		this.score.visible = true;
+		this.stop.visible = true;
+	}
+
+	function onshezhiClick(){
+		this.titile1.visible = false;
+		this.titile2.visible = false;
+		this.titile3.visible = false;
+		this.titile4.visible = false;
+		this.gamestart.visible = false;
+		this.shezhi.visible = false;
+
+		this.turn3.visible = true;
+		this.jia.visible = true;
+		this.jian.visible = true;
+		this.label1.visible = true;
+	}
+
+	function onturn3Click(){
+		this.titile1.visible = true;
+		this.titile2.visible = true;
+		this.titile3.visible = true;
+		this.titile4.visible = true;
+		this.gamestart.visible = true;
+		this.shezhi.visible = true;
+
+		this.turn3.visible = false;
+		this.jia.visible = false;
+		this.jian.visible = false;
+		this.label1.visible = false;
+	}
+
+	function onjiaClick(){
+		this.label1.text = "出现2的概率：" + ( * 10) + "%";
+	}
+
+	function onjianClick(){
+
+	}
+}
+
+Laya.class(TestUI, "TestUI", TestPageUI);
+
+Laya.MiniAdpter.init();
+
+Laya.init(400, 500, WebGL);
+
+Laya.ResourceVersion.enable("version.json", Handler.create(null, beginLoad), Laya.ResourceVersion.FILENAME_VERSION);
+
+function beginLoad(){
+	Laya.loader.load("res/atlas/comp.atlas", Handler.create(null, onLoaded));
+	Laya.loader.load("res/music.wav", Handler.create(null, onmusic));
+}
+
+var Testui;
+
+function onLoaded(){
+	Testui = new TestUI();
+	Laya.stage.addChild(Testui);
+}
+
+function onmusic(){
+	Laya.SoundManager.playMusic( "res/music.wav", 0 );
+}
---
+++ src/bin/js/ui/layaUI.max.all.js
@@ -0,0 +1,41 @@
+var CLASS$=Laya.class;
+var STATICATTR$=Laya.static;
+var View=laya.ui.View;
+var Dialog=laya.ui.Dialog;
+var TestPageUI=(function(_super){
+		function TestPageUI(){
+			
+		    this.gamestart=null;
+		    this.titile4=null;
+		    this.titile3=null;
+		    this.titile2=null;
+		    this.titile1=null;
+		    this.score=null;
+		    this.box=null;
+		    this.stop=null;
+		    this.gamescore=null;
+		    this.box2=null;
+		    this.again=null;
+		    this.turn1=null;
+		    this.turn2=null;
+		    this.label1=null;
+		    this.jia=null;
+		    this.turn3=null;
+		    this.jian=null;
+		    this.shezhi=null;
+
+			TestPageUI.__super.call(this);
+		}
+
+		CLASS$(TestPageUI,'ui.test.TestPageUI',_super);
+		var __proto__=TestPageUI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(TestPageUI.uiView);
+
+		}
+
+		TestPageUI.uiView={"type":"View","props":{"y":0,"x":0,"width":400,"height":500},"child":[{"type":"Image","props":{"y":0,"x":0,"width":400,"skin":"comp/background.png","height":600}},{"type":"Button","props":{"y":250,"x":0,"width":400,"var":"gamestart","labelStroke":3,"labelSize":35,"label":"开始","height":100}},{"type":"Label","props":{"y":50,"x":315,"width":70,"var":"titile4","text":"8","height":200,"fontSize":80,"color":"#06D6A0"}},{"type":"Label","props":{"y":50,"x":230,"width":70,"var":"titile3","text":"4","height":200,"fontSize":80,"color":"#1B9AAA"}},{"type":"Label","props":{"y":50,"x":140,"width":70,"var":"titile2","text":"0","height":200,"fontSize":80,"color":"#EF476F"}},{"type":"Label","props":{"y":50,"x":50,"width":70,"var":"titile1","text":"2","height":200,"fontSize":80,"color":"#FFC43D"}},{"type":"Label","props":{"x":0,"width":200,"visible":false,"var":"score","top":20,"text":"分数：","fontSize":40,"color":"#080808","bold":true,"align":"center"}},{"type":"Box","props":{"y":100,"x":0,"width":400,"visible":false,"var":"box","height":400}},{"type":"Button","props":{"y":-10,"x":315,"width":100,"visible":false,"var":"stop","labelStroke":2,"labelSize":35,"labelAlign":"center","label":"暂停","height":100}},{"type":"Label","props":{"y":25,"x":0,"width":400,"visible":false,"var":"gamescore","text":"label","height":100,"fontSize":60,"color":"#010101","align":"center"}},{"type":"Box","props":{"y":0,"x":0,"width":400,"visible":false,"var":"box2","height":500},"child":[{"type":"Button","props":{"y":250,"x":0,"width":400,"visible":true,"var":"again","labelStroke":2,"labelSize":35,"labelAlign":"center","label":"再来一局","height":100}},{"type":"Button","props":{"y":350,"x":0,"width":400,"visible":true,"var":"turn1","labelStroke":2,"labelSize":35,"labelAlign":"center","label":"主菜单","height":100}}]},{"type":"Button","props":{"y":150,"x":0,"width":400,"visible":false,"var":"turn2","labelStroke":2,"labelSize":35,"labelAlign":"center","label":"回到游戏","height":100}},{"type":"Label","props":{"y":100,"x":0,"width":400,"visible":false,"var":"label1","text":"出现2的概率：50%","height":100,"fontSize":40,"align":"center"}},{"type":"Button","props":{"y":300,"x":0,"width":200,"visible":false,"var":"jia","labelSize":25,"label":"出现2的概率增加","height":100}},{"type":"Button","props":{"y":350,"x":0,"width":400,"visible":false,"var":"turn3","labelStroke":2,"labelSize":35,"labelAlign":"center","label":"主菜单","height":100}},{"type":"Button","props":{"y":300,"x":200,"width":200,"visible":false,"var":"jian","labelSize":25,"label":"出现2的概率降低","height":100}},{"type":"Button","props":{"y":350,"x":0,"width":400,"visible":true,"var":"shezhi","labelStroke":3,"labelSize":35,"label":"设置","height":100}}]};
+		return TestPageUI;
+	})(View);
